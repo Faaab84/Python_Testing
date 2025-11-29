e8dd30d9fd9f04fcd9464abecb6af9a46b6c52b3
Faaab84 <fabien.balaramane@outlook.fr>
2025-11-29 16:36:39 +0100

    bug/club_should_not_able_book_more_the_competition_place fix #7

--- server.py
+++ server.py
@@ -50,7 +50,7 @@ def purchasePlaces():
     competition = next((c for c in competitions if c['name'] == request.form['competition']), None)
     club = next((c for c in clubs if c['name'] == request.form['club']), None)
 
-    if not club or not competition:
+    if not competition or not club:
         flash("Something went wrong – please try again")
         return render_template('welcome.html', club=club or {}, competitions=competitions)
 
@@ -59,30 +59,33 @@ def purchasePlaces():
         if comp_date < datetime.now():
             flash("You cannot book places for a past competition.")
             return render_template('welcome.html', club=club, competitions=competitions)
-    except (ValueError, KeyError):
+    except (ValueError, KeyError, TypeError):
         flash("Something went wrong – please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
 
-    try:
-        placesRequired = int(request.form['places'])
-        if placesRequired <= 0:
-            raise ValueError
-    except ValueError:
+    places_str = request.form['places'].strip()
+
+    if not places_str or not places_str.isdigit() or int(places_str) <= 0:
         flash("Please enter a valid number of places.")
         return render_template('welcome.html', club=club, competitions=competitions)
 
+    placesRequired = int(places_str)
     club_points = int(club['points'])
+    places_available = int(competition['numberOfPlaces'])
 
     if placesRequired > 12:
         flash("You cannot book more than 12 places per competition.")
     elif placesRequired > club_points:
-        flash(f"Not enough points – you only have {club_points} points available.")
+        flash(f"Not enough points – you only have {club_points}.")
+    elif placesRequired > places_available:
+        flash(f"Not enough places available – only {places_available} left.")
     else:
-        competition['numberOfPlaces'] = str(int(competition['numberOfPlaces']) - placesRequired)
-        club['points'] = str(club_points - placesRequired)     #bug 5 fix with #bug 2
+        competition['numberOfPlaces'] = str(places_available - placesRequired)
+        club['points'] = str(club_points - placesRequired)
         flash("Great-booking complete!")
 
     return render_template('welcome.html', club=club, competitions=competitions)
+
 
 
 # TODO: Add route for points display
